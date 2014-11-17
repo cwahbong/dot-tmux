bc8b3f683fadaae51373c0755e0913dbc54a8075
cwahbong <cw.ahbong@gmail.com>
2014-11-17 21:34:26 +0800

    Handle old symlink.

--- config.sh
+++ config.sh
@@ -1,17 +1,23 @@
 #!/usr/bin/env sh
-set -efu
+set -u
 
-TMUX_DEFAULT_PREFIX=${HOME}"/.tmux"
+TMUX_DEFAULT_PREFIX="${HOME}/.tmux"
 
 CONFIG_INSTALL () {
 	TMUX_PREFIX=${1:-$TMUX_DEFAULT_PREFIX}
-	TMUX_CONF=$HOME/.tmux.conf
-	ln -s ${TMUX_PREFIX}/tmux.conf $HOME/.tmux.conf
+	TMUX_CONF="$HOME/.tmux.conf"
+
+	if [ -L "$TMUX_CONF" ]; then
+		rm "$TMUX_CONF"
+		echo "Found old symlink and removed it..."
+	fi
+
+	ln -s "${TMUX_PREFIX}/tmux.conf" "$TMUX_CONF" \
+		&& echo "Conf installed succesfully."
 }
 
 CONFIG_HELP () {
 	echo 'Usage: config.sh {install [prefix_path]|help}'
-	return $(($? || $#))
 }
 
 if [ $# -gt 0 ]; then
@@ -27,6 +33,6 @@ case ${OP:-} in
 		;;
 	*)
 		CONFIG_HELP $@
-		exit $(($? || 1))
+		exit 1
 		;;
 esac
